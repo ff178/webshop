c410219164209f1bab4c9af4bff8ba258876ddce
Martiolo Jimenez <martioloJimenez@MacBook-de-Martiolo.local>
2017-01-06 20:08:40 -0500

    termi

--- views/Usuario/checkout.php
+++ views/Usuario/checkout.php
@@ -14,21 +14,21 @@
             <div class="check">
                 <div class="container">
                     <div class="col-md-3 cart-total">
-                        <a class="continue" href="#">Continuar Comprando</a>
+                        <a class="continue" href="<?php print(URL); ?>Productos/products">Continuar Comprando</a>
                         <div class="price-details">
-                            <h3>Price Details</h3>
+                            <h3>Detalles del Precio</h3>
                         </div>
                         <hr class="featurette-divider">
                         <ul class="total_price">
                            <li class="last_price"> <h4>TOTAL</h4></li>
-                           <li class="last_price"><span>6150.00</span></li>
+                           <li class="last_price"><span class="simpleCart_total"></span></li>
                            <div class="clearfix"> </div>
                         </ul>
                         <div class="clearfix"></div>
                         <a class="order" href="#">Realizar Compra</a>
                     </div>
                     <div class="col-md-9 cart-items">
-                        <h1>Mi Carrito de Compras (2)</h1>
+                        <h1>Mi Carrito de Compras (<span id="simpleCart_quantity" class="simpleCart_quantity"></span>)</h1>
                             <script>$(document).ready(function(c) {
                                 $('.close1').on('click', function(c){
                                     $('.cart-header').fadeOut('slow', function(c){
@@ -37,28 +37,7 @@
                                     });
                                 });
                            </script>
-                        <div class="cart-header">
-                <div class="close1"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
-                            <div class="cart-sec simpleCart_shelfItem">
-                                    <div class="cart-item cyc">
-                                        <img src="images/grid8.jpg" class="img-responsive" alt=""/>
-                                    </div>
-                                   <div class="cart-item-info">
-                                        <ul class="qty">
-                                            <li><p>Size : 9 US</p></li>
-                                            <li><p>Qty : 1</p></li>
-                                            <li><p>Price each : $190</p></li>
-                                        </ul>
-                                        <div class="delivery">
-                                             <p>Service Charges : Rs.190.00</p>
-                                             <span>Delivered in 2-3 bussiness days</span>
-                                             <div class="clearfix"></div>
-                                        </div>
-                                   </div>
-                                   <div class="clearfix"></div>
-
-                              </div>
-                         </div>
+                        
                          <script>$(document).ready(function(c) {
                                 $('.close2').on('click', function(c){
                                         $('.cart-header2').fadeOut('slow', function(c){
